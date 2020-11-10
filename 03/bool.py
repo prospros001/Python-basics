710888632a5815c6e6f440e15867c0c2c82798b6
prospros001 <prospros@naver.com>
2020-11-10 14:32:08 +0900

    수치형 연습

---
+++ 03/bool.py
@@ -0,0 +1,31 @@
+# 부울(boolean) 참이나 거짓을 나타내는 값
+# True, False 두 리터럴만 가진다.
+
+b0 = True
+b1 = False
+
+a = 1
+b = 2
+c = a * b
+print(a, b, c)
+
+b3 = a > b
+
+print(b3, type(b3))
+
+b4 = b3 * 10
+print(b4)
+
+print(True * False)
+print(True or False)
+
+a = 1
+print(a > 10)
+print(a < 10)
+
+b = a == 1
+print(type(b))
+
+print(b + 10)
+print(True + True)
+
