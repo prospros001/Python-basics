96fea3fc9123cbe2b945f5ee22a0f381ee33133c
prospros001 <prospros@naver.com>
2020-11-11 15:50:19 +0900

    SET 연습

--- 03/set.py
+++ 03/set.py
@@ -14,13 +14,50 @@ print(words, type(words))
 word_list2 = list(words)
 print(word_list2, type(word_list2))
 
-# for w in words:
-#     count = word_list.count(w)
-#     print(f'{w}:{count}')
+for w in words:
+    count = word_list.count(w)
+    print(f'{w}:{count}')
 
 print('---- 객체함수 ----')
+s.add(7)
+print(s)
 
+s.add(2)   #중복안된다.
+print(s)
 
+s.discard(2)
+print(s)
 
+s.remove(1)
+print(s)
 
+s.discard(10)
+print(s)
+
+try:
+    s.remove(10)
+except KeyError as e:
+    print('remove는 discard와 다르게 없는 원소 제거시 예외 발생')
+
+s.update({6, 7, 8})  # 추가할때 사용
+print(s)
+
+s.clear()
+print(s)
+
+print('---- 객체함수 : 수학 ----')
+s1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+s2 = {10, 20, 30, 40, 50}
+
+s3 = s1.union(s2)  # 합집합
+print(s3)
+
+s4 = s1.intersection(s2)  #교집합
+print(s4)
+
+s5 = s1.difference(s2)    # 차집합
+print(s5)
+
+s6 = s1.symmetric_difference(s2)    # 대칭차
+print(s6)
 
