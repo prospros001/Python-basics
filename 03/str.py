6239b5e573d084a8d06a24d7aa131c2f02269f93
prospros001 <prospros@naver.com>
2020-11-11 11:21:14 +0900

    문자열 메서드

--- 03/str.py
+++ 03/str.py
@@ -65,6 +65,48 @@ print('----------- 문자열 객체는 변경할수 없다(immutable) ----------
 print('----------- 문자열 연산: 슬라이싱 -------------')
 
 print('----------- 문자열 연산: 객체 함수-------------')
+# 대소문자 관련 함수
+s = 'i like Python'
+print(s.upper())
+print(s.lower())
+print(s.swapcase())     # 대소문자를 바꾼다.
+print(s.capitalize())   # 가장 앞만 대문자로
+print(s.title())        # 단어 앞글자만 대문자
+
+# 검색 관련 함수
+s = 'I Like Python, I Like Deep Learning Also'
+print(s.count("Like"))
+print(s.find("Like"))
+print(s.find("Like", 5))
+print(s.find("AI"))               # find()함수는 못찾으면 -1처리
+print(s.rfind("Like"))
+
+try :
+    print(s.index("AI"))
+except ValueError as e:
+    print('index() 함수는 발견하지 못하면 예외가 발생한다.')
+
+print(s.rindex("Like"))
+print(s.startswith("i Like"))
+print(s.endswith("Also"))
+print(s.endswith("Python", 0, 13))
+
+# 편집 및 치환
+s = '        spam and ham         '
+print('---' + s.strip() + '-----')        #전후 공백 없앤다
+print('---' + s.rstrip() + '-----')        #오른쪽 공백 없앤다
+print('---' + s.lstrip() + '-----')        #왼쪽 공백 없앤다
+
+s = '<><><abc><><defg><><>'
+print('---' + s.strip('<>') + '-----')
+print(s.replace('<>', ''))
+
+# 분리
+s = 'one:two:three'
+r = s.split(':')
+print(r, type(r))
+
 
 print('----------- 문자열 연산: 포맷팅 -------------')
+
 
