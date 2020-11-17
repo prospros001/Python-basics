c0f97625de9d0b08d9b3643b35c9a8fa642cad22
prospros001 <prospros@naver.com>
2020-11-17 17:55:05 +0900

    최종 #1

--- 04_ok/object-copy.py
+++ 04_ok/object-copy.py
@@ -1,8 +1,8 @@
 # 객체 카피
 
-# 레퍼런스 카피
 import copy
 
+print("===== 레퍼런스 복사======")
 a = [1, 2, 3]
 b = [4, 5, 6]
 x = [a, b, 100]
@@ -12,7 +12,7 @@ print(x)
 print(y)
 print(x is y)
 
-print('==== swallow copy(얕은복사) =====')
+print("=====swallow copy(앝은 복사)======")
 a = [1, 2, 3]
 b = [4, 5, 6]
 x = [a, b, 100]
@@ -23,7 +23,7 @@ print(y)
 print(x is y)
 print(x[0] is y[0])
 
-print('==== deep copy(깊은 복사) =====')
+print("=====deep copy(깊은 복사)======")
 a = [1, 2, 3]
 b = [4, 5, 6]
 x = [a, b, 100]
@@ -34,8 +34,10 @@ print(y)
 print(x is y)
 print(x[0] is y[0])
 
-print('==== 복합객체가 한개만 있는 경우 깊은/얖은 카피는 별차이 없다. =====')
-a = ['hello', 'world']
+
+print("=====복합객체가 한개만 있는 경우 깊은/앝은 카피는 별 차이가 없다.======")
+a = ["hello", "world"]
+
 b = copy.copy(a)
 print(a is b)
 print(a[0] is b[0])
@@ -43,3 +45,6 @@ print(a[0] is b[0])
 c = copy.deepcopy(a)
 print(a is c)
 print(a[0] is c[0])
+
+
+a = [[1, 2, 3], [4, 5, 6]]
